7502fef0b4a56a9fb6c7ccc946f2982d20ec08fd
Al Moreno <83099648+wrongpixels@users.noreply.github.com>
2025-04-24 18:48:33 +0200

    Exercise 11.12

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.get('/health', (req, res) => {
   res.send('ok')
 })
 
-app.listen(PORT + 1, () => {
+app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
 })
