95c6cb5b20122d7992f2290d76aecd3a2619cb05
Al Moreno <83099648+wrongpixels@users.noreply.github.com>
2025-04-24 16:22:13 +0200

    Exercise 11.10

--- e2e-tests/pokedex.spec.js
+++ e2e-tests/pokedex.spec.js
@@ -6,4 +6,10 @@ describe('Pokedex', () => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('character names are trademarks')).toBeVisible()
   })
+  test('we can go to a specific page', async ({ page }) => {
+    await page.goto('')
+    await page.getByText('ivysaur').click()
+    await expect(page).toHaveURL(/pokemon\/ivysaur$/)
+    await expect(page.getByText('chlorophyll')).toBeVisible()
+  })
 })
